9b8900ca7d32768342b53ccda78db19c0d21e30b
root <root@DuckHandsome.>
2024-12-18 17:34:26 +0700

    Update code: Implement momentum strategy without finding abnormal momentum stocks

--- src/backtest/backtest.py
+++ src/backtest/backtest.py
@@ -2,199 +2,65 @@ import pandas as pd
 import numpy as np
 from portfolio.portfolio import Portfolio
 from mom_signal.mom_signal import MomentumSignal
+from strategy.strategy import AbnormalMomentumStatey
 
 from alive_progress import alive_bar
 
 class Backtest(Portfolio, MomentumSignal):  
-    cum_top_excess_w_ret = []
-    cum_top_excess_l_ret = []
-    cum_top_excess_arb_ret = []
-
-    cum_bottom_excess_w_ret = []
-    cum_bottom_excess_l_ret = []
-    cum_bottom_excess_arb_ret = []
-
-    winner_top_port_list = []
-    loser_top_port_list = []
-
-    loser_bottom_port_list = []
-    winner_bottom_port_list = []
-
-    win_counts_top = []
-    lose_counts_top = []
-    
-    win_counts_bottom = []
-    lose_counts_bottom = []
-    
     def __init__(self, portfolio: Portfolio):
             self.portfolio = portfolio 
-    
+            self.performance_dfs = []
+            self.portfolio_features = []
+
     def run(self): 
         past_formation_dfs, future_holding_dfs = self.portfolio.get_walk_forward_splits()
         formed_portfolio_list = self.portfolio.get_formed_portfolio()
-        
-        future_holding_idx = pd.concat(future_holding_dfs).index
-        cap_groups = ['Top', 'Bottom']
-        portfolio_groups = ['Winner', 'Loser', 'Arbitrage']
-        mom_groups = ['CMOM', 'MMOM', 'TMOM', 'PMOM']
-        
-        performance_idx = pd.MultiIndex.from_product([
-                future_holding_idx,
-                cap_groups,
-                portfolio_groups,
-                mom_groups
-        ])
-        
-        performance_df = pd.DataFrame(np.nan, index=performance_idx, columns=['r'])
             
         # Run Backtest
         print("Backtest is running ...")
         with alive_bar(len(past_formation_dfs), force_tty=True) as bar: 
-            for i in range(len(past_formation_dfs)):
-                # Get formation period
+            for past_formation_period, future_holding_period, selected_stocks in zip(past_formation_dfs, future_holding_dfs, formed_portfolio_list):
+                
+                # Get formed portfolio
                 mask_fields = ['r', 'benchmark', 'log_mcap']
-                df_selected = past_formation_dfs[i].stack(future_stack=True).loc[self.idx[:, formed_portfolio_list[i]], :]
-                df_selected = df_selected[mask_fields]
+                formed_portfolio = past_formation_period.stack(future_stack=True).loc[self.idx[:, selected_stocks], :]
+                formed_portfolio = formed_portfolio[mask_fields]
 
                 # Calculate Momentum Mesures
-                df_selected['CMOM'] = df_selected.groupby(level=1)['r'].transform(lambda x: self.calculate_cmom(x))
-                df_selected['MMOM'] = df_selected.groupby(level=1)['r'].transform(lambda x: self.calculate_mmom(x))
-                df_selected['TMOM'] = df_selected.groupby(level=1)['r'].transform(lambda x: self.calculate_tmom(x))
-                df_selected['PMOM'] = self.calculate_pmom(df_selected)
-
-                # Fomation Portfolio
-                N = len(formed_portfolio_list[i])
-
-                N_top = N_bottom =round(.3*N)
+                formed_portfolio['CMOM'] = formed_portfolio.groupby(level=1)['r'].transform(lambda x: self.calculate_cmom(x))
+                formed_portfolio['MMOM'] = formed_portfolio.groupby(level=1)['r'].transform(lambda x: self.calculate_mmom(x))
+                formed_portfolio['TMOM'] = formed_portfolio.groupby(level=1)['r'].transform(lambda x: self.calculate_tmom(x))
+                formed_portfolio['PMOM'] = self.calculate_pmom(formed_portfolio)
                 
-                top_30 = df_selected.dropna().nlargest(N_top, 'log_mcap')
-                bottom_30 = df_selected.dropna().nsmallest(N_bottom, 'log_mcap')
-
-                mom_top_30 = top_30[['CMOM', 'MMOM', 'TMOM', 'PMOM']]
-                mom_bottom_30 = bottom_30[['CMOM', 'MMOM', 'TMOM', 'PMOM']]
-
-                # cap = df_selected.dropna()[['log_mcap']]
-
-                avg_top_30 = mom_top_30.mean()
-                avg_bottom_30 = mom_bottom_30.mean()
-
-                weights_top_30 = (mom_top_30
-                        .sub(avg_top_30)
-                        .div(N_top)
-                        .rename(
-                                columns={
-                                        'CMOM': 'CMOM_top_weight',
-                                        'MMOM': 'MMOM_top_weight',
-                                        'TMOM': 'TMOM_top_weight',
-                                        'PMOM': 'PMOM_top_weight'
-                                }
-                        ))
-
-                weights_bottom_30 = (mom_bottom_30
-                        .sub(avg_bottom_30)
-                        .div(N_bottom)
-                        .rename(
-                                columns={
-                                        'CMOM': 'CMOM_bottom_weight',
-                                        'MMOM': 'MMOM_bottom_weight',
-                                        'TMOM': 'TMOM_bottom_weight',
-                                        'PMOM': 'PMOM_bottom_weight'
-                                }
-                        ))
-
-                winner_top_portfolio = weights_top_30.map(lambda x: x if x > 0 else 0)
-                winner_bottom_portfolio = weights_bottom_30.map(lambda x: x if x > 0 else 0)
-
-                loser_top_portfolio = weights_top_30.map(lambda x: abs(x) if x < 0 else 0)
-                loser_bottom_portfolio = weights_bottom_30.map(lambda x: abs(x) if x < 0 else 0)
+                formed_portfolio = formed_portfolio.dropna()
                 
-                self.winner_top_port_list.append(winner_top_portfolio)
-                self.winner_bottom_port_list.append(winner_bottom_portfolio)
-                self.loser_top_port_list.append(loser_top_portfolio)
-                self.loser_bottom_port_list.append(loser_bottom_portfolio)
-
-
-                # Calculate future holding return
-                ret = future_holding_dfs[i].loc[:, self.idx[:, formed_portfolio_list[i]]].stack(future_stack=True)[['r']]
-                benchmark = future_holding_dfs[i]['benchmark'].iloc[0, 0]
-
-                top_ret = ret.loc[self.idx[:, top_30.index.get_level_values(1)], :]
-                bottom_ret = ret.loc[self.idx[:, bottom_30.index.get_level_values(1)], :]
-
-                w_top_ret = (winner_top_portfolio
-                        .mul(top_ret.values.reshape(N_top, 1), axis='columns')
-                        .mul(100)
-                        .rename(columns={
-                                        'CMOM_weight': 'CMOM_top_ret',
-                                        'MMOM_weight': 'MMOM_top_ret',
-                                        'TMOM_weight': 'TMOM_top_ret',
-                                        'PMOM_weight': 'PMOM_top_ret'
-                                        }                
-                        )) # Unit: percentage
-
-                w_bottom_ret = (winner_bottom_portfolio
-                        .mul(bottom_ret.values.reshape(N_bottom, 1), axis='columns')
-                        .mul(100)
-                        .rename(columns={
-                                        'CMOM_weight': 'CMOM_bottom_ret',
-                                        'MMOM_weight': 'MMOM_bottom_ret',
-                                        'TMOM_weight': 'TMOM_bottom_ret',
-                                        'PMOM_weight': 'PMOM_bottom_ret'
-                                        }                
-                        )) # Unit: percentage
-
-                l_top_ret = (loser_top_portfolio
-                        .mul(top_ret.values.reshape(N_top, 1), axis='columns')
-                        .mul(100)
-                        .rename(columns={
-                                        'CMOM_weight': 'CMOM_top_ret',
-                                        'MMOM_weight': 'MMOM_top_ret',
-                                        'TMOM_weight': 'TMOM_top_ret',
-                                        'PMOM_weight': 'PMOM_top_ret'
-                                        }                
-                        )) # Unit: percentange
-
-                l_bottom_ret = (loser_bottom_portfolio
-                        .mul(bottom_ret.values.reshape(N_bottom, 1), axis='columns')
-                        .mul(100)
-                        .rename(columns={
-                                        'CMOM_weight': 'CMOM_bottom_ret',
-                                        'MMOM_weight': 'MMOM_bottom_ret',
-                                        'TMOM_weight': 'TMOM_bottom_ret',
-                                        'PMOM_weight': 'PMOM_bottom_ret'
-                                        }                
-                        )) # Unit: percentange
-
-                arb_top_ret = ((w_top_ret - l_top_ret)
-                        .rename(columns={
-                                        'CMOM_weight': 'CMOM_top_ret',
-                                        'MMOM_weight': 'MMOM_top_ret',
-                                        'TMOM_weight': 'TMOM_top_ret',
-                                        'PMOM_weight': 'PMOM_top_ret'  
-                                        }
-                        ))  # Unit: percentage
-
-                arb_bottom_ret = ((w_bottom_ret - l_bottom_ret)
-                        .rename(columns={
-                                        'CMOM_weight': 'CMOM_bottom_ret',
-                                        'MMOM_weight': 'MMOM_bottom_ret',
-                                        'TMOM_weight': 'TMOM_bottom_ret',
-                                        'PMOM_weight': 'PMOM_bottom_ret'  
-                                        }
-                        ))  # Unit: percentage
-
-                w_top_excess_ret = w_top_ret.sum() #- benchmark
-                l_top_excess_ret = l_top_ret.sum() #- benchmark
-                arb_top_excess_ret = arb_top_ret.sum() #- benchmark
+                self.portfolio_features.append(formed_portfolio)
                 
-                w_bottom_excess_ret = w_bottom_ret.sum() #- benchmark
-                l_bottom_excess_ret = l_bottom_ret.sum() #- benchmark
-                arb_bottom_excess_ret = arb_bottom_ret.sum() #- benchmark
-                self.cum_top_excess_w_ret.append(w_top_excess_ret.values)
-                self.cum_top_excess_l_ret.append(l_top_excess_ret.values)
-                self.cum_top_excess_arb_ret.append(arb_top_excess_ret.values)
+                #################################################
+                ##                                             ##
+                ##        IMPLEMENT HDBSCAN HERE!!!!!!         ##
+                ##                                             ##
+                #################################################
                 
-                self.cum_bottom_excess_w_ret.append(w_bottom_excess_ret.values)
-                self.cum_bottom_excess_l_ret.append(l_bottom_excess_ret.values)
-                self.cum_bottom_excess_arb_ret.append(arb_bottom_excess_ret.values)
+                
+                # Divide stocks in the portfolio into 3 market capitalization groups and for each group, we use only stocks with abnormal momentum to construct our portfolio
+                formed_portfolio['cap_groups'] = formed_portfolio['log_mcap'].transform(lambda x: pd.qcut(x, [0, .3, .7, 1], labels=['bottom', 'mid', 'top']))
+                formed_portfolio[['cmom_weights', 'mmom_weights', 'tmom_weights', 'pmom_weights']] = formed_portfolio.groupby('cap_groups', observed=True)[['CMOM', 'MMOM', 'TMOM', 'PMOM']].transform(lambda x: (1/len(x) * (x - x.mean())))
+                
+                # Get future return
+                future_return = future_holding_period.loc[:, self.idx[:, selected_stocks]].stack(future_stack=True)[['r']]
+
+                drop_cols = ['r', 'benchmark', 'CMOM', 'MMOM', 'TMOM', 'PMOM']
+                
+                # Create performance dataframe
+                performance = (formed_portfolio.drop(columns=drop_cols)
+                                        .droplevel(0)
+                                        .reset_index()
+                                        .merge(future_return.reset_index(), how='inner', left_on='ticker', right_on='ticker')
+                                        .set_index(['time', 'ticker', 'cap_groups'])
+                        
+                              )
+                
+                self.performance_dfs.append(performance)
+                
                 bar() 
--- src/strategy/strategy.py
+++ src/strategy/strategy.py
@@ -1,2 +1,40 @@
 import pandas as pd
+import numpy as np
+from hdbscan import HDBSCAN
+from sklearn.preprocessing import StandardScaler
 
+class AbnormalMomentumStatey: 
+    def __hyperparameter_search(scaled_feature): 
+        results = []
+
+        min_sample_grid = np.arange(5, 35)
+        min_cluster_size_grid = np.arange(5, 35)
+
+        for min_sample in min_sample_grid: 
+            for min_cluster_size in min_cluster_size_grid: 
+                
+                clf = HDBSCAN(min_samples=min_sample, min_cluster_size=min_cluster_size)
+                clf.fit(scaled_feature)
+                
+                if len(clf.cluster_persistence_) == 0:
+                    continue
+                
+                results.append([min_sample, min_cluster_size, clf.cluster_persistence_.mean()]) 
+        
+        return sorted(results, key=lambda x : x[-1], reverse=True)[0]
+    
+    @classmethod
+    def find_cluster(self, formed_portfolio): 
+        feature_cols = ['CMOM', 'MMOM', 'TMOM', 'PMOM']
+        
+        feature = formed_portfolio[feature_cols].values
+
+        sc = StandardScaler()
+        scaled_feature = sc.fit_transform(feature)
+        
+        best_min_sample, best_min_cluster_size, _ = self.__hyperparameter_search(scaled_feature)
+        
+        clf = HDBSCAN(min_samples=best_min_sample, min_cluster_size=best_min_cluster_size)
+        clf.fit(scaled_feature)
+        
+        return clf.labels_
